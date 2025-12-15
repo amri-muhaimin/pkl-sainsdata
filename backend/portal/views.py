290ecd662a23f15aa0e9cd1cfbc4344c53ae906d
amri-muhaimin <amri.muhaimin.stat@upnjatim.ac.id>
2025-12-15 16:48:48 +0700

    Merge for last version

--- backend/portal/views.py
+++ backend/portal/views.py
@@ -11,6 +11,7 @@ from .views_dosen import (
     dosen_seminar_list,
     dosen_seminar_detail,
     dosen_seminar_penilaian,
+    dosen_pembimbing_penilaian,
     seminar_penilaian_pdf,
     koordinator_dashboard,
     koordinator_pendaftaran_list,
